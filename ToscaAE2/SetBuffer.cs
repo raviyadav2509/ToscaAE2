225a5e168de8411fae153468678c58ede34b67d8
Ravi Yadav <ravs_avs@yahoo.com>
2021-01-13 19:09:12 +1100

    initial commit

---
+++ ToscaAE2/SetBuffer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tricentis.Automation.AutomationInstructions.Configuration;
+using Tricentis.Automation.AutomationInstructions.Dynamic.Values;
+using Tricentis.Automation.AutomationInstructions.TestActions;
+using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines.SpecialExecutionTasks;
+using Tricentis.Automation.Engines.SpecialExecutionTasks.Attributes;
+using Tricentis.Automation.Execution.Results;
+
+namespace ToscaAE2
+{
+    [SpecialExecutionTaskName("SetBuffer")]
+    public class SetBuffer : SpecialExecutionTaskEnhanced
+    {
+        public SetBuffer(Validator validator) : base(validator)
+        {
+        }
+
+        public override void ExecuteTask(ISpecialExecutionTaskTestAction testAction)
+        {
+            //Iterate over each TestStepValue
+            foreach (IParameter parameter in testAction.Parameters)
+            {
+                //ActionMode input means set the buffer
+                if (parameter.ActionMode == ActionMode.Input)
+                {
+                    IInputValue inputValue = parameter.GetAsInputValue();
+                    Buffers.Instance.SetBuffer(parameter.Name, inputValue.Value, false);
+                    testAction.SetResultForParameter(parameter, SpecialExecutionTaskResultState.Ok, string.Format("Buffer {0} set to value {1}.", parameter.Name, inputValue.Value));
+                }
+                //Otherwise we let TBox handle the verification. Other ActionModes like WaitOn will lead to an exception.
+                else
+                {
+                    //Don't need the return value of HandleActualValue in this case.
+                    HandleActualValue(testAction, parameter, Buffers.Instance.GetBuffer(parameter.Name));
+                }
+            }
+            string buffer = Buffers.Instance.GetBuffer("Buff1");
+            Console.WriteLine("The value of the buffer read using GetBuffer method is " + buffer);
+            Buffers.Instance.SetBuffer("Buff1", "NewValue", false);
+        }
+    }
+}
---
+++ ToscaAE2/StartProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Tricentis.Automation.AutomationInstructions.Configuration;
+using Tricentis.Automation.AutomationInstructions.Dynamic.Values;
+using Tricentis.Automation.AutomationInstructions.TestActions;
+using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines;
+using Tricentis.Automation.Engines.SpecialExecutionTasks;
+using Tricentis.Automation.Engines.SpecialExecutionTasks.Attributes;
+
+namespace ToscaAE2
+{
+
+    [SpecialExecutionTaskName("StartProgram")]
+    public class StartProgram : SpecialExecutionTask
+    {
+
+        public StartProgram(Validator validator) : base(validator) { }
+
+        public override ActionResult Execute(ISpecialExecutionTaskTestAction testAction)
+        {
+            // get the input values of the teststep, created with XModule >StartProgram<
+            IInputValue path = testAction.GetParameterAsInputValue((string)"Path", (bool)false);
+            IParameter parameter = testAction.GetParameter((string)"Arguments", (bool)true);
+            string processArguments = string.Empty;
+
+            // Arguments has sub parameters – retrieve and concatenate them
+            if (parameter != null)
+            {
+                IEnumerable<IParameter> arguments = parameter.GetChildParameters((string)"Argument");
+
+                foreach (IParameter argument in arguments)
+                {
+                    IInputValue processArgument = argument.Value as IInputValue;
+                    processArguments += processArgument.Value + " ";
+                }
+            }
+
+            Process.Start(path.Value, processArguments);
+
+
+            // return, that the action worked
+            return new PassedActionResult("Started successfully");
+        }
+    }
+}
